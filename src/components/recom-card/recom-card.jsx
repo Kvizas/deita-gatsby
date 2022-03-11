10a925d726cdf32da303e69a9c06dc9c5ffbcdc9
Kvizas <theqxizgaming@gmail.com>
2022-03-11 13:35:18 +0200

    Recommendation

---
+++ src/components/recom-card/recom-card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import "./recom-card.sass";
+
+import Icon from "../../images/announce_icon.svg";
+
+export default function RecomCard({ recom }) {
+
+  const linksnis = () => {
+    if (recom.amount % 10 === 0) return "pamokų";
+    if (recom.amount % 10 === 1) return "pamoka"; 
+    else return "pamokos"; 
+  }
+
+  return (
+    <div className='recomcard'>
+      <div className="recomcard__icon">
+        <img src={Icon} alt={recom.title} />
+      </div>
+      <div className="recomcard__content">
+        <h5>{recom.title}</h5>
+        <div className="recomcard__subtitle">
+          {recom.subtitle}
+        </div>
+        <div className="recomcard__amount">
+          <span className="recomcard__amount__rect">
+            {recom.amount}
+          </span>
+          {linksnis()}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/recommendation/recommendation.jsx
+++ src/components/recommendation/recommendation.jsx
@@ -4,28 +4,58 @@ import "./recommendation.sass"
 
 import Shadow from "../../images/shadow.svg";
 import LeftConfetty from "../../images/left-confetty.svg";
-import FieldCard from '../field-card/field-card';
+import RightConfetty from "../../images/right-confetty.svg";
+import RecomCard from '../recom-card/recom-card';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function Recommendation() {
   return (
-    <section className='recom__wrapper' style={{background: `url(${Shadow}), linear-gradient(.25turn, var(--secondary-color) 50%, var(--primary-color) 50%)`}}>
+    <section className='recom'>
+      <h2>
+        <span className='primary'>Deita</span> mokymosi platforma jums
+        <br/>rekomenduoja
+      </h2>
 
-      <div className='recom'>
-        <h2>
-          <span className='primary'>Deita</span> mokymosi platforma jums
-          <br/>rekomenduoja
-        </h2>
+      <div className='recom__side__wrapper' style={{background: `url(${Shadow}), linear-gradient(.25turn, var(--secondary-color) 50%, var(--primary-color) 50%)`}}>
 
-        <img className="recom__side recom__side--left" src={LeftConfetty} alt="Dekoracijos" />
         
         <div className="recom__container">
-          <div className="recom__content">
-            <FieldCard field={{
+
+          <img className="recom__side recom__side--left" src={LeftConfetty} alt="Dekoracijos" />
+          <img className="recom__side recom__side--right" src={RightConfetty} alt="Dekoracijos" />
+
+          <StaticImage
+            className="recom__img"
+            src='../../images/bethoven.svg'
+            placeholder='tracedSVG'
+            width={400}
+          />
+
+            <RecomCard recom={{
+              title: "Matematika",
+              subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+              amount: 210
+            }}></RecomCard>
+            <RecomCard recom={{
+              title: "Matematika",
+              subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+              amount: 3215
+            }}></RecomCard>
+            <RecomCard recom={{
               title: "Matematika",
               subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
               amount: 42
-            }}></FieldCard>
-          </div>
+            }}></RecomCard>
+            <RecomCard recom={{
+              title: "Matematika",
+              subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+              amount: 1456
+            }}></RecomCard>
+            <RecomCard recom={{
+              title: "Matematika",
+              subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
+              amount: 651
+            }}></RecomCard>
         </div>
       </div>
     </section>
